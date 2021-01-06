36e540b5f5d3ddc21eef2add0042cae3fbed2638
Angelia Beek <72133720+angeliabeek@users.noreply.github.com>
2021-01-06 15:39:27 +0100

    Add files via upload

---
+++ Opdr1_gebruik_eerste_JavaScript/script.js
@@ -0,0 +1,45 @@
+console.log("Hello Winc Academy");
+
+let name = 'Angelia Beek';
+console.log(name);
+
+//let name = 'If'; console.log(name); In Chrome Dev Tools: SyntaxError: Identifier 'name' has already been declared//
+
+let x = 4 + 4;
+console.log(x);
+
+let text1 = "4";
+let text2 = "4";
+let text3 = text1 + text2;
+console.log(text3);
+
+let a = 3;
+let b = 2;
+let c = a + b;
+console.log(c);
+
+let d = 3;
+let e = 2;
+let f = d - e;
+console.log(f);
+
+let g = 3;
+let h = 2;
+let i = g * h;
+console.log(i);
+
+let j = 6
+let k = 3
+let l = j / k;
+console.log(l);
+
+let m = 6;
+let n = 4;
+let o = m % n;
+console.log(o);
+
+let age = 30;
+console.log(age);
+
+let text = 'dertig';
+console.log(text);
